3218bc5391ae326e35bcd092b33fceb52e119787
Onolax <meenamonu2002@gmail.com>
2026-03-28 18:52:47 +0000

    chore: service stubs, all 6 modules

---
+++ game-data-service/src/main/java/com/pokemon/gamedata/GameDataServiceApplication.java
@@ -0,0 +1,11 @@
+package com.pokemon.gamedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDataServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GameDataServiceApplication.class, args);
+    }
+}
